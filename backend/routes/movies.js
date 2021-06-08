1b42df6dcdb3b442692d1c5b1547cd0d9a25fb7f
Faour Manon <manon.faour@student-cs.fr>
2021-06-08 15:15:54 +0200

    route to fetch movies

---
+++ backend/routes/movies.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const MovieModel = require("../models/movie");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  MovieModel.find({}).then(function (movies) {
+    res.json({ movies: movies });
+  });
+});
+
+router.post("/new", function (req, res) {
+  const newMovie = new MovieModel({
+    title: req.body.title,
+    description: req.body.description,
+    release_date: req.body.release_date,
+    id_tmdb: req.body.id_tmdb,
+    poster_path: req.body.poster_path,
+    average_rating: req.body.average_rating,
+    genre_ids: req.body.genre_ids,
+  });
+
+  newMovie
+    .save()
+    .then(function (newDocument) {
+      res.status(201).json(newDocument);
+    })
+    .catch(function (error) {
+      res.status(500).json({ message: error });
+    });
+});
+
+module.exports = router;
