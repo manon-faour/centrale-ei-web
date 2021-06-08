521caf3852e3e23e55a59c87465a6afb166e4dec
Faour Manon <manon.faour@student-cs.fr>
2021-06-08 14:47:35 +0200

    user model + delete user

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   firstName: { type: String },
   lastName: { type: String },
+  password: { type: String },
+  myFilms: [{ type: mongoose.Types.ObjectId, ref: 'Movies' }],
 });
 
 const UserModel = mongoose.model("UserModel", UserSchema, "users");
