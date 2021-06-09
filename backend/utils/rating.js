c7b624093762c5a5fafbf224bc00326c8a10b58e
Faour Manon <manon.faour@student-cs.fr>
2021-06-09 17:16:03 +0200

    add ratings and top voted films

---
+++ backend/utils/rating.js
@@ -0,0 +1,18 @@
+const MovieModel = require("../models/movie");
+const EvalModel = require("../models/eval");
+
+const average = async function (movieId) {
+  const evals = await EvalModel.find({ movie: movieId });
+  let note = 0;
+  var i;
+  for (i = 0; i < evals.length; i++) {
+    note = note + evals[i].eval;
+  }
+  const number = evals.length;
+  const movie = await MovieModel.findOne({ _id: movieId });
+  console.log(note / number);
+  movie.average_rating = note / number;
+  movie.save();
+};
+
+exports.average = average;
